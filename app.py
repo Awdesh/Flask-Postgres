ff89a0000f49c58d62e1bc1f8a2ba1ea39ce0b3b
Awdesh Sharma <arafel5@gmail.com>
2016-11-03 00:13:06 +0000

    adding get and post on backend

--- app.py
+++ app.py
@@ -2,10 +2,8 @@ import os
 import requests
 import operator
 import re
-import nltk
 from flask import Flask, render_template, request
 from flask.ext.sqlalchemy import SQLAlchemy
-# from stop_words import stops
 from collections import Counter
 from bs4 import BeautifulSoup
 
@@ -17,71 +15,43 @@ from worker import conn
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# binding the db instance to the flask app
 db = SQLAlchemy(app)
 
 from models import Device
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+@app.route('/addDevice', methods=['GET', 'POST'])
+def addDevice():
+    print 'posting now..'
+    errors = []
+    try:
+        device = Device(
+            imei=1234566,
+            name="dev_device",
+            realm="dev",
+            serial=12345,
+            sim=3153454567
+        )
+        db.session.add(device)
+        print device
+        print device.id
+        print device.name
+        print device.sim
+        db.session.commit()
+    except:
+        errors.append("Unable to add items to the database")
     return render_template('index.html')
-
-
-Port = int(os.getenv('PORT'))
-Ip = os.getenv('IP')
-
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
     
-# if __name__ == '__main__':
-#     app.run(Ip,Port)
-
-
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     errors = []
-#     results = {}
-#     if request.method == "POST":
-#         # get url that the person has entered
-#         try:
-#             url = request.form['url']
-#             r = requests.get(url)
-#         except:
-#             errors.append(
-#                 "Unable to get URL. Please make sure it's valid and try again."
-#             )
-#             return render_template('index.html', errors=errors)
-#         if r:
-#             # text processing
-#             raw = BeautifulSoup(r.text, 'html.parser').get_text()
-#             nltk.data.path.append('./nltk_data/')  # set the path
-#             tokens = nltk.word_tokenize(raw)
-#             text = nltk.Text(tokens)
-#             # remove punctuation, count raw words
-#             nonPunct = re.compile('.*[A-Za-z].*')
-#             raw_words = [w for w in text if nonPunct.match(w)]
-#             raw_word_count = Counter(raw_words)
-#             # stop words
-#             no_stop_words = [w for w in raw_words if w.lower() not in stops]
-#             no_stop_words_count = Counter(no_stop_words)
-#             # save the results
-#             results = sorted(
-#                 no_stop_words_count.items(),
-#                 key=operator.itemgetter(1),
-#                 reverse=True
-#             )[:10]
-#             try:
-#                 result = Result(
-#                     url=url,
-#                     result_all=raw_word_count,
-#                     result_no_stop_words=no_stop_words_count
-#                 )
-#                 db.session.add(result)
-#                 db.session.commit()
-#             except:
-#                 errors.append("Unable to add item to database.")
-#     return render_template('index.html', errors=errors, results=results)
-
-
+@app.route('/getDevice', methods=['GET'])    
+def getDevice():
+    errors = []
+    print 'retrieving a device'
+    try:
+        devices = db.session.query(Device).all()
+        print len(devices)
+    except:
+        errors.append("Unable to retrieve items from the database")
+    return render_template("index.html")
+    
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -7,10 +7,16 @@ class Device(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     imei = db.Column(db.Integer)
     name = db.Column(db.String)
+    serial = db.Column(db.Integer)
+    sim = db.Column(db.Integer)
+    realm = db.Column(db.String)
 
-    def __init__(self, imei, name):
+    def __init__(self, imei, name, realm, serial, sim):
         self.imei = imei
         self.name = name
+        self.serial = serial
+        self.sim = sim
+        self.realm = realm
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
