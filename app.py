8b604eb5331b8dc3987d5c30ebc23f67e41d8c34
Awdesh Sharma <arafel5@gmail.com>
2016-11-08 04:42:35 +0000

    adding device,patient table

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import requests
 import operator
 import re
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from flask.ext.sqlalchemy import SQLAlchemy
 from collections import Counter
 
@@ -18,6 +18,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from models import Device
+from models import Patient
+from models import Sim
 
 @app.route('/addDevice', methods=['GET','POST'])
 def addDevice():
@@ -25,23 +27,57 @@ def addDevice():
     errors = []
     if request.method == 'POST':
         print 'in here'
-        device = Device(request.form['imei'], request.form['deviceName'], request.form['serial'], request.form['sim'], request.form['realm'])
+        device = Device(int(request.form['imei']), request.form['deviceName'], int(request.form['serial']), int(request.form['sim']), request.form['realm'])
         db.session.add(device)
         db.session.commit()
+        flash('you have successfully created item.')
     return render_template('new.html')
+    
+@app.route('/addPatient', methods=['GET','POST'])
+def addPatient():
+    print 'posting now..'
+    errors = []
+    if request.method == 'POST':
+        print 'in here'
+        patient = Patient((request.form['firstName']), request.form['lastName'], (request.form['title']), (request.form['gender']), request.form['address1'])
+        db.session.add(patient)
+        db.session.commit()
+        flash('you have successfully created item.')
+    return render_template('newpatient.html')
     
 @app.route('/', methods=['GET'])    
 def getDevices():
     errors = []
     print 'retrieving devices'
-    return render_template('index.html', devices = db.session.query(Device).all())
+    devices = db.session.query(Device).all()
+    if len(devices) < 0:
+        flash('There are no items yet.')
+        return
+    else:
+        flash('Below are the devices.')
+        return render_template('index.html', devices = devices)
     
 @app.route('/devices/<int:device_id>', methods=['GET'])    
 def getDevice(device_id):
     print 'retrieving a device'
-    devices = db.session.query(Device).get(device_id)
-    print devices.imei
+    print device_id
+    device = db.session.query(Device).get(device_id)
+    if device is None:
+        flash('There is no device with this Id.')
+    print device.imei
     return render_template('index.html', device = db.session.query(Device).get(device_id))
+    
+@app.route('/devices/<string:realm>', methods=['GET'])    
+def getDeviceByRealm(realm):
+    print 'retrieving a device from imei'
+    device = Device.query.filter_by(realm=realm).first_or_404()
+    return render_template('index.html', device = device)
+    
+@app.route('/getPatients', methods=['GET'])    
+def getPatients():
+    errors = []
+    print 'retrieving patients'
+    return render_template('patient.html', patients = db.session.query(Patient).all())
     
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -5,11 +5,12 @@ class Device(db.Model):
     __tablename__ = 'device'
 
     id = db.Column(db.Integer, primary_key=True)
-    imei = db.Column(db.Integer)
+    imei = db.Column(db.BigInteger)
     name = db.Column(db.String)
-    serial = db.Column(db.Integer)
-    sim = db.Column(db.Integer)
+    serial = db.Column(db.BigInteger)
+    sim = db.Column(db.BigInteger)
     realm = db.Column(db.String)
+    status = db.Column(db.String)
 
     def __init__(self, imei, name, serial, sim, realm):
         self.imei = imei
@@ -17,6 +18,49 @@ class Device(db.Model):
         self.serial = serial
         self.sim = sim
         self.realm = realm
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+        
+class Patient(db.Model):
+    __tablename__ = 'patient'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    title = db.Column(db.String)
+    gender = db.Column(db.String)
+    height = db.Column(db.String)
+    address1 = db.Column(db.String)
+    address2 = db.Column(db.String)
+    city = db.Column(db.String)
+    
+    def __init__(self, first_name, last_name, title, gender, address1):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.title = title
+        self.gender = gender
+        self.address1 = address1
+        # self.address2 = address2
+        # self.city = city
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+        
+class Sim(db.Model):
+    __tablename = 'sim'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    imsi = db.Column(db.String)
+    caller_id_number = db.Column(db.String)
+    iccid = db.Column(db.String)
+    network_operator = db.Column(db.String)
+    
+    def __init__(self, imsi, caller_id_number, iccid, network_operator):
+        self.imsi = imsi
+        self.caller_id_number = caller_id_number
+        self.iccid = iccid
+        self.network_operator = network_operator
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
